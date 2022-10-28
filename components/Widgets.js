5655c9ca42728de26be06c77059dde3359d52fd1
AsimMc <amasimmalik@gmail.com>
2022-10-28 12:23:11 -0400

    test

--- components/Widgets.js
+++ components/Widgets.js
@@ -8,7 +8,7 @@ import dynamic from 'next/dynamic'
 export default function Widgets({newsResults}) {
   const[articleNum, setArticleNum] = useState(3);
   return (
-    <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+    <div className="xl:w-[600px] lg:inline ml-8 space-y-5">
         <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
             <div className="flex items-center p-3 rounded-full bg-red-300 relative">
                 <SearchIcon className="h-5 z-50 text-gray-500 "/>
