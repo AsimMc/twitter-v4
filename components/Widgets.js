be05e46c0cf6eb0d2e1dfe7c8584a3c4ecc24081
AsimMc <amasimmalik@gmail.com>
2022-10-31 16:09:19 -0400

    test

--- components/Widgets.js
+++ components/Widgets.js
@@ -9,27 +9,48 @@ import {useEffect} from "react";
 
 export default function Widgets({newsResults}) {
   const[articleNum, setArticleNum] = useState(3);
-  const[progressValue, setProgressValue] = useState(0);
+  const[progressValue1, setProgressValue1] = useState(0);
+  const[progressValue2, setProgressValue2] = useState(0);
+  const[progressValue3, setProgressValue3] = useState(0);
 
-let progressEndValue = 0,
-  progressStartValue = 0,
-  speed = 40;
+  let progressEndValue1 = 94, progressEndValue2 = 84, progressEndValue3 = 44,
+  progressStartValue1 = 0, progressStartValue2 = 0, progressStartValue3 = 0;
+
 useEffect(() => {
-  const progress = setInterval(() =>{
-    if(progressEndValue > progressStartValue){
-      progressStartValue++;
-      setProgressValue(progressStartValue);
+  const progress1 = setInterval(() =>{
+    if(progressEndValue1 > progressStartValue1){
+      progressStartValue1++;
+      setProgressValue1(progressStartValue1);
+      console.log(progressValue1)
     }
     else{
-      clearInterval(progress);
+      clearInterval(progress1);
     }
-  }, speed);
+  }, 40);
+  const progress2 = setInterval(() =>{
+    if(progressEndValue2 > progressStartValue2){
+      progressStartValue2++
+      setProgressValue2(progressStartValue2);
+    }
+    else{
+      clearInterval(progress2);
+    }
+  }, 40);
+  const progress3 = setInterval(() =>{
+    if(progressEndValue3 > progressStartValue3){
+      progressStartValue3++;
+      setProgressValue3(progressStartValue3);
+    }
+    else{
+      clearInterval(progress3);
+    }
+  }, 40);
 }, []);
   return (
     <div className="xl:w-[600px] lg:inline ml-8 space-y-5">
         <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
             <div className="flex items-center p-3 rounded-full bg-red-300 relative">
-                <SearchIcon className="h-5 z-50 text-gray-500 "/>
+              <SearchIcon className="h-5 z-50 text-gray-500 "/>
                 <input className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100" type="text" placeholder="Search RUbars" />
             </div>
         </div>
@@ -48,16 +69,16 @@ useEffect(() => {
         <div className="text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
           <h4 className="font-bold text-xl px-4">Capacity</h4>
           <div className="container">
-            <div className="circular-progress" style={{background: `conic-gradient(#E0115F ${progressValue * 3.6}deg, #ededed 0deg)`}}>
-            <span className="progress-value">{progressValue}%</span>
+            <div className="circular-progress" style={{background: `conic-gradient(#E0115F ${progressValue1 * 3.6}deg, #ededed 0deg)`}}>
+            <span className="progress-value">{progressValue1}%</span>
             </div>
             <span className="text">Olde Queens</span>
-              <div className="circular-progress" style={{background: `conic-gradient(#E0115F ${progressValue * 3.6}deg, #ededed 0deg)`}}>
-              <span className="progress-value">{progressValue}%</span>
+              <div className="circular-progress" style={{background: `conic-gradient(#E0115F ${progressValue2 * 3.6}deg, #ededed 0deg)`}}>
+              <span className="progress-value">{progressValue2}%</span>
             </div>
             <span className="text1">Golden Rail</span>
-            <div className="circular-progress1" style={{background: `conic-gradient(#E0115F ${progressValue * 3.6}deg, #ededed 0deg)`}}>
-              <span className="progress-value1">{progressValue}%</span>
+            <div className="circular-progress1" style={{background: `conic-gradient(#E0115F ${progressValue3 * 3.6}deg, #ededed 0deg)`}}>
+              <span className="progress-value1">{progressValue3}%</span>
             </div>
             <span className="text">Huey&apos;s Knight Club</span>
             <button className="text-rose-300 pl-4 pb-3 hover:text-rose-400">Show more</button>
