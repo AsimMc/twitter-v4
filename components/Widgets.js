f275c9b6c3e37867f15810e1082bb2268ad81ab4
AsimMc <amasimmalik@gmail.com>
2022-11-08 22:11:46 -0500

    yolo

--- components/Widgets.js
+++ components/Widgets.js
@@ -13,7 +13,7 @@ export default function Widgets({newsResults}) {
   const[progressValue2, setProgressValue2] = useState(0);
   const[progressValue3, setProgressValue3] = useState(0);
 
-  let progressEndValue1 = 6, progressEndValue2 = 2, progressEndValue3 = 3,
+  let progressEndValue1 = 6, progressEndValue2 = 3, progressEndValue3 = 3,
   progressStartValue1 = 0, progressStartValue2 = 0, progressStartValue3 = 0;
 
 useEffect(() => {
