d3ed00932a912c26d4f72df1f2f2eb1ae7ced617
AsimMc <amasimmalik@gmail.com>
2022-11-16 19:40:39 -0500

    test

--- components/Widgets.js
+++ components/Widgets.js
@@ -13,7 +13,7 @@ export default function Widgets({newsResults}) {
   const[progressValue2, setProgressValue2] = useState(0);
   const[progressValue3, setProgressValue3] = useState(0);
 
-  let progressEndValue1 = 78, progressEndValue2 = 92, progressEndValue3 = 78,
+  let progressEndValue1 = 0, progressEndValue2 = 0, progressEndValue3 = 0,
   progressStartValue1 = 0, progressStartValue2 = 0, progressStartValue3 = 0;
 
 useEffect(() => {
